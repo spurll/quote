ddd71477f69c2f9f8cfdb087b67f9dcbe7354862
Gem Newman <spurll@gmail.com>
2016-01-25 14:15:10 -0600

    Slight cleanup of width specification in format_plaintext.

--- quote/controller.py
+++ quote/controller.py
@@ -6,7 +6,7 @@ import textwrap
 from quote import app, contents
 
 
-def get(root=app.config['DEFAULT_ROOT'], output_format='plain', width=None):
+def get(root=app.config['DEFAULT_ROOT'], output_format='plain', width=''):
     item = random.choice(contents[root])
 
     if output_format == 'html':
@@ -60,45 +60,41 @@ def format_html(root, item):
     )
 
 
-def format_plaintext(root, item, width=None):
+def format_plaintext(root, item, width=''):
     if root == 'poem':
         output = '{}\n{}\n\n{}'.format(
             item.get('work'), item.get('author'), item.get('content')
         )
 
     else:
-        format_width = '{{:>{}}}'.format(width) if width is not None else '{}'
         attribution = ''
 
         if 'character' in item:
-            if 'author' in item:
-                attribution = (format_width + '\n' + format_width).format(
-                    '—{}'.format(item['character']),
-                    'in {} by {}'.format(item.get('work'), item['author'])
-                )
-            else:
-                attribution = (format_width + '\n' + format_width).format(
-                    '—{}'.format(item.get('character')), 'in ' + item['work']
-                )
-        elif 'work' in item:
-            attribution = (format_width + '\n' + format_width).format(
-                '—{},'.format(item.get('author')), item['work']
+            author = (
+                'in {} by {}'.format(item.get('work'), item['author'])
+                if 'author' in item else 'in {}'.format(item.get('work'))
             )
-        elif 'author' in item:
-            if item.get('dubious'):
-                attribution = format_width.format(
-                    'Generally attributed to ' + item['author']
-                )
-            else:
-                attribution = format_width.format('—' + item['author'])
 
-        output = (
-            item.get('content') +
-            ('\n{}'.format(attribution) if attribution else '') +
-            ('\n{}'.format(item['link']) if 'link' in item else '')
+            attribution = '{:>{width}}\n{:>{width}}'.format(
+                '—' + item['character'], author, width=width
+            )
+
+        elif 'work' in item:
+            attribution = '{:>{width}}\n{:>{width}}'.format(
+                '—' + item.get('author') + ',', item['work'], width=width
+            )
+
+        elif 'author' in item:
+            attribution = '{:>{width}}'.format(
+                ('Generally attributed to ' if item.get('dubious') else '—')
+                + item['author'], width=width
+            )
+
+        output = item.get('content') + (
+            '\n{}'.format(attribution) if attribution else ''
         )
 
-    if width is not None:
+    if width:
         lines = map(
             lambda x: textwrap.fill(x, width=width, replace_whitespace=False),
             output.splitlines()
